8deac286b0cbe178636b18367181f52991a08cfe
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-01-29 15:48:38 -0600

    added sign up page

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,10 +3,8 @@ import { Router } from "express";
 
 var router = Router();
 
-
 router.post("/login", auth.login);
-
-
+router.post("/signup", auth.signup);  // ADD THIS LINE
 router.post("/logout", auth.logout);
 
 export default router;
