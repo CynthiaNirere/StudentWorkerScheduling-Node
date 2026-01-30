41805675c5b98d06e110c45913adae9d14f995d8
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-01-30 13:06:49 -0600

    worked on auth controller

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,7 +4,7 @@ import { Router } from "express";
 var router = Router();
 
 router.post("/login", auth.login);
-router.post("/signup", auth.signup);  // ADD THIS LINE
+//router.post("/signup", auth.signup);  // ADD THIS LINE
 router.post("/logout", auth.logout);
 
 export default router;
