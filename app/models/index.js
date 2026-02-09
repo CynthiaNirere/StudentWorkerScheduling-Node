50e0ebe5f77fb34f1e1836a576f931635da45658
tessy.mugisha <mugishatessy@gmail.com>
2026-02-08 21:36:01 -0600

    addcoverage-backend

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,6 @@
 import User from "./user.model.js";
 import Session from "./session.model.js";  // Make sure this exists
+import Coverage from "./coverage.models.js";
 import sequelize from "../config/sequelizeInstance.js";
 import { Sequelize } from "sequelize";
 
@@ -11,5 +12,5 @@ db.sequelize = sequelize;
 // Models
 db.user = User;
 db.session = Session;  // Make sure this is here
-
+db.coverage = Coverage;
 export default db;
---
+++ app/routes/coverage.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import * as coverage from "../controllers/coverage.controller.js";
+import authenticate from "../authorization/authorization.js";
+
+const router = express.Router();
+
+router.post("/", authenticate, coverage.create);
+router.get("/", authenticate, coverage.findAll);
+router.get("/:id", authenticate, coverage.findOne);
+router.put("/:id", authenticate, coverage.update);
+router.delete("/:id", authenticate, coverage.remove);
+
+// Additional routes specific to coverage
+router.get("/location/:locationId", authenticate, coverage.findByLocation);
+router.get("/jobrole/:jobRoleId", authenticate, coverage.findByJobRole);
+router.get("/location/:locationId/day/:dayOfWeek", authenticate, coverage.findByLocationAndDay);
+
+export default router;
