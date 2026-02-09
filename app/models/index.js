3fa4123e1086012e031cc394cd811f6ed20168cc
LewisUwase <lewis.uwase@eagles.oc.edu>
2026-02-09 14:13:03 -0600

    Add-backendTablesforShifts

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,11 @@
 import User from "./user.model.js";
 import Session from "./session.model.js";  // Make sure this exists
+import Shift from "./shift.model.js";
+import TimeOffRequest from "./Timeoffrequest.model.js";
+import ShiftSwapRequest from "./Shiftswaprequest.model.js";
+import ShiftSwapRequestUser from "./shiftSwapRequestUser.model.js";
+import TaskList from "./taskList.model.js";
+import TaskListItem from "./taskListItem.model.js";
 import sequelize from "../config/sequelizeInstance.js";
 import { Sequelize } from "sequelize";
 
@@ -10,6 +16,12 @@ db.sequelize = sequelize;
 
 // Models
 db.user = User;
-db.session = Session;  // Make sure this is here
+db.session = Session;  
+db.shift = Shift;
+db.timeOffRequest = TimeOffRequest;
+db.shiftSwapRequest = ShiftSwapRequest;
+db.shiftSwapRequestUser = ShiftSwapRequestUser;
+db.taskList = TaskList;
+db.taskListItem = TaskListItem;
 
 export default db;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
+import shiftRoutes from "./shift.routes.js";
+import timeOffRequestRoutes from "./Timeoffrequest.routes.js";
+import shiftSwapRequestRoutes from "./Shiftswaprequest.routes.js";
+import taskListRoutes from "./taskList.routes.js";
+import taskListItemRoutes from "./taskListItem.routes.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/shifts", shiftRoutes);
+router.use("/time-off-requests", timeOffRequestRoutes);
+router.use("/shift-swap-requests", shiftSwapRequestRoutes);
+router.use("/task-lists", taskListRoutes);
+router.use("/task-list-items", taskListItemRoutes);
 
 export default router;
