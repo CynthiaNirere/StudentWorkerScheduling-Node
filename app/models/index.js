b652b9b60e94cb372100a78b0aad2f8839482683
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-02-09 14:43:31 -0600

    resolved-routes-and-modelsindex

--- app/models/index.js
+++ app/models/index.js
@@ -7,6 +7,11 @@ import Session from "./session.model.js";  // Make sure this exists
 import Coverage from "./coverage.models.js";
 import Notification from "./notifications.models.js";
 import Skill from "./skills.models.js";
+import TaskListItem from "./taskListItem.model.js";
+import TaskList from "./taskList.model.js";
+import ShiftSwapRequest from "./shiftSwapRequest.model.js";
+import TimeOffRequest from "./timeOffRequest.model.js";
+
 import sequelize from "../config/sequelizeInstance.js";
 import { Sequelize } from "sequelize";
 
@@ -27,4 +32,9 @@ db.session = Session;  // Make sure this is here
 db.coverage = Coverage;
 db.notification = Notification;
 db.skill = Skill;// Make sure this is here
+db.taskListItem = TaskListItem;
+db.taskList = TaskList;
+db.shiftSwapRequest = ShiftSwapRequest;
+db.timeOffRequest = TimeOffRequest;
+
 export default db;
