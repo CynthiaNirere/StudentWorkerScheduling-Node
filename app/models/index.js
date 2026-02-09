87bf75162bce4b4b1d28243784fa91754d379510
tessy.mugisha <mugishatessy@gmail.com>
2026-02-09 14:10:38 -0600

    add skills

--- app/models/index.js
+++ app/models/index.js
@@ -1,8 +1,9 @@
 import User from "./user.model.js";
 import Session from "./session.model.js";  // Make sure this exists
 import Coverage from "./coverage.models.js";
+import Notification from "./notifications.models.js";
+import Skill from "./skills.models.js";
 import sequelize from "../config/sequelizeInstance.js";
-import admin from "./admin.model.js";
 import { Sequelize } from "sequelize";
 
 const db = {};
@@ -14,6 +15,6 @@ db.sequelize = sequelize;
 db.user = User;
 db.session = Session;  // Make sure this is here
 db.coverage = Coverage;
-db.admin = admin;
-db.coverage = Coverage;
+db.notification = Notification;
+db.skill = Skill;// Make sure this is here
 export default db;
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,7 @@ import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
 import coverageRoutes from "./coverage.routes.js";
 import notificationRoutes from "./notifications.routes.js";
-import adminRoutes from "./admin.routes.js";
-import coverageRoutes from "./coverage.routes.js";
-import notificationRoutes from "./notifications.routes.js";
+import skillRoutes from "./skills.routes.js";
 
 const router = express.Router();
 
@@ -13,7 +11,5 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/coverage", coverageRoutes);
 router.use("/notifications", notificationRoutes);
-router.use("/admin", adminRoutes);
-router.use("/coverage", coverageRoutes);
-router.use("/notifications", notificationRoutes);
+router.use("/skills", skillRoutes);//here we add the skill routes
 export default router;
