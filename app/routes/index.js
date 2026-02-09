17b9d63d552200adb93a532764f046c319349cc3
tessy.mugisha <mugishatessy@gmail.com>
2026-02-08 21:51:05 -0600

    add nootifications backend

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
 import coverageRoutes from "./coverage.routes.js";
+import notificationRoutes from "./notifications.routes.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/coverage", coverageRoutes);
-
+router.use("/notifications", notificationRoutes);
 export default router;
