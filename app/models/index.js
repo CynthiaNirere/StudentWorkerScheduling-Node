41234da2546fa6e78a2c08b0f5c15ebffa78d0f1
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-02-16 09:45:04 -0600

    updated index.js

--- app/models/index.js
+++ app/models/index.js
@@ -10,6 +10,8 @@ import TaskListItem from "./taskListItem.model.js";
 import TaskList from "./taskList.models.js";
 import ShiftSwapRequest from "./shiftSwapRequest.model.js";
 import TimeOffRequest from "./timeOffRequest.model.js";
+import BusinessArea from "./businessArea.model.js"; 
+import JobRole from "./jobRole.model.js";
 
 import sequelize from "../config/sequelizeInstance.js";
 import { Sequelize } from "sequelize";
@@ -25,6 +27,15 @@ db.session = Session;
 db.schedule = Schedule;
 db.availability = Availability;
 db.clock = Clock;
+db.coverage = Coverage;
+db.notification = Notification;
+db.skill = Skill;
+db.taskListItem = TaskListItem;
+db.taskList = TaskList;
+db.shiftSwapRequest = ShiftSwapRequest;
+db.timeOffRequest = TimeOffRequest;
+db.businessArea = BusinessArea; 
+db.jobRole = JobRole; 
 
 // Associations
 User.hasMany(Session, { foreignKey: "user_id", onDelete: "CASCADE" });
@@ -35,12 +46,17 @@ Availability.belongsTo(User, { foreignKey: "user_id" });
 
 User.hasMany(Clock, { foreignKey: "user_id", onDelete: "CASCADE" });
 Clock.belongsTo(User, { foreignKey: "user_id" });
-db.coverage = Coverage;
-db.notification = Notification;
-db.skill = Skill;// Make sure this is here
-db.taskListItem = TaskListItem;
-db.taskList = TaskList;
-db.shiftSwapRequest = ShiftSwapRequest;
-db.timeOffRequest = TimeOffRequest;
+// NEW: BusinessArea and JobRole Associations
+BusinessArea.hasMany(JobRole, {
+  foreignKey: 'location_id',
+  sourceKey: 'location_id',
+  as: 'jobRoles'
+});
+
+JobRole.belongsTo(BusinessArea, {
+  foreignKey: 'location_id',
+  targetKey: 'location_id',
+  as: 'businessArea'
+});
 
 export default db;
