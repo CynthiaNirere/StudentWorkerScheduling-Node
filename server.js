911afed8706002d50e6a91279d1c0a8875aeaae1
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-02-27 13:44:22 -0600

    changed server

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const corsOptions = {
     "http://localhost:8081",
     "http://localhost:5173",
     "https://workerscheduling.eaglesoftwareteam.com",
-    "https://workerscheduling.eaglesoftwareteam.com:3131",
-    "https://project3.eaglesoftwareteam.com" 
+    "https://workerscheduling.eaglesoftwareteam.com:3131"
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ["Content-Type", "Authorization", "x-requested-with"],
