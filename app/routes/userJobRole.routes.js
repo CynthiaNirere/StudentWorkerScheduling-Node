7c611de80986b5b46b0c2f2670528c0a1e37d08c
MagnaniFabiola <magnanfabiola@gmail.com>
2026-04-08 19:28:40 -0500

    let the role be assigned to a specific location

--- app/routes/userJobRole.routes.js
+++ app/routes/userJobRole.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as userJobRole from "../controllers/userJobRole.controller.js";
 import authenticate from "../authorization/authorization.js";
+import attachLocation from "../authorization/attachLocation.js";
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ const router = express.Router();
 router.post("/user/:userId/roles", authenticate, userJobRole.addRoleToUser);
 router.get("/user/:userId/roles", authenticate, userJobRole.getUserRoles);
 router.delete("/user/:userId/roles/:roleId", authenticate, userJobRole.removeRoleFromUser);
-router.put("/user/:userId/roles/:roleId/primary", authenticate, userJobRole.setPrimaryRole);
+router.put("/user/:userId/roles/:roleId/primary", authenticate, attachLocation, userJobRole.setPrimaryRole);
 
 // ✅ Get users by role
 router.get("/role/:roleId/users", authenticate, userJobRole.getUsersByRole);
